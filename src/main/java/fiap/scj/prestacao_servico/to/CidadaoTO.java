41cf644924fa662428ea9333050e24a25e4a57a5
Eduardo Ferreira Galego <156711@prodesp-sede.local>
2016-10-24 20:15:50 -0200

    First commit

---
+++ src/main/java/fiap/scj/prestacao_servico/to/CidadaoTO.java
@@ -0,0 +1,64 @@
+package fiap.scj.prestacao_servico.to;
+
+public class CidadaoTO {
+
+	private String registro;
+	private String nomeCidadao;
+	private String telefone;
+	private String email;
+	private String observacoes;
+
+	public CidadaoTO(String registro, String nomeCidadao, String telefone, String email, String observacoes) {
+		super();
+		this.registro = registro;
+		this.nomeCidadao = nomeCidadao;
+		this.telefone = telefone;
+		this.email = email;
+		this.observacoes = observacoes;
+	}
+
+	public String getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+
+	public String getNomeCidadao() {
+		return nomeCidadao;
+	}
+
+	public void setNomeCidadao(String nomeCidadao) {
+		this.nomeCidadao = nomeCidadao;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	@Override
+	public String toString() {
+		return "DadosCidadaoTO [registro=" + registro + ", nomeCidadao=" + nomeCidadao + ", telefone=" + telefone + ", email=" + email + ", observacoes=" + observacoes + "]";
+	}
+}
